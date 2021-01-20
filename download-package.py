ce53d0207add4435a9fe169e4ddf1a888d131e75
Your Name <you@example.com>
2021-01-20 12:55:06 +0100

    fixed glitch

--- download-package.py
+++ download-package.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
 import os
 package='helloworld'
 repo='https://raw.githubusercontent.com/toto112358/pypack-repo/master/packages'
 branch='stable'
 url=repo+'/'+branch+'/'+package+'.zst'
 print(url)
-os.system('mkdir test && cd test')
-os.system(f'curl -Os {url} | zstdcat | sh')
+os.system('mkdir test')
+os.system(f'cd test && curl {url} | zstdcat | sh')
