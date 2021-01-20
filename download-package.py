0f600b06c8a19bd41f5ff6c1a61e6a08233b3556
Your Name <you@example.com>
2021-01-20 13:02:28 +0100

    fixed self extract

--- download-package.py
+++ download-package.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 import os
+cd = os.chdir
 package='helloworld'
 repo='https://raw.githubusercontent.com/toto112358/pypack-repo/master/packages'
 branch='stable'
 url=repo+'/'+branch+'/'+package+'.zst'
 print(url)
 os.system('mkdir test')
-os.system(f'cd test && curl {url} | zstdcat | sh')
+cd('test')
+os.system(f'curl {url} | zstdcat | sh')
